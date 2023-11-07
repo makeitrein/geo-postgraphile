fe898cecf501e0ed1533fc2e0dfb38c6dd071f9d
badgeth-dao <81835284+badgeth-dao@users.noreply.github.com>
2023-11-07 11:14:01 -0800

    Simplify Inflector for prettier Graphql Names

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const http = require("http");
 const { postgraphile } = require("postgraphile");
+const PgOrderByRelatedPlugin = require("@graphile-contrib/pg-order-by-related");
+const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
+const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
+
 require("dotenv").config();
 
 http
   .createServer(
     postgraphile(process.env.DATABASE_URL, "public", {
+      schemas: [],
       watchPg: true,
       graphiql: true,
       enhanceGraphiql: true,
+      // graphileBuildOptions: {
+      //   connectionFilterRelations: true, // default: false
+      // },
+      appendPlugins: [PgOrderByRelatedPlugin, ConnectionFilterPlugin, PgSimplifyInflectorPlugin],
     })
   )
   .listen(process.env.PORT || 5000);
